eecb1025e4a7db92ec7bed5f891d72f2ae5a637c
HOGIE Luc <luc.hogie@inria.fr>
2021-01-09 10:34:42 +0100

    improved description

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-*Idawi* is a Java decentralized middleware for distributed applications.
+*Idawi* is a Java decentralized middleware for distributed applications. It is developed at the Computer Science Laboratory of the Universté Côte d'Azur (Nice, France), which is a joint lab of Cnrs (Centre National de la Recherche Scientific) and Inria Sophia Antipolis.
 It is extensively described in the on-going working paper there:
 https://sharelatex.irisa.fr/read/drmkyvwckyzz
 
--- src/main/java/idawi/Operation.java
+++ src/main/java/idawi/Operation.java
@@ -7,11 +7,15 @@ public abstract class Operation {
 	double totalDuration;
 	protected final OperationDescriptor descriptor;
 	final Class declaringClass;
-	
+
 	public Operation(Class declaringClass) {
 		this.descriptor = new OperationDescriptor();
 		this.descriptor.impl = getClass().getName();
 		this.declaringClass = declaringClass;
+	}
+
+	public boolean isSystemOperation() {
+		return declaringClass != Service.class;
 	}
 
 	public abstract String getName();
@@ -32,7 +36,7 @@ public abstract class Operation {
 	}
 
 	public OperationDescriptor descriptor() {
-		this.descriptor.name  = getName();
+		this.descriptor.name = getName();
 		this.descriptor.nbCalls = this.nbCalls;
 		this.descriptor.totalDuration = this.totalDuration;
 		return descriptor;
