099a948f34bb0be350a9d8baebdc64076f226b31
Lidaou Denis Assoti <denisassoti@yahoo.com>
2023-06-09 14:43:19 +0200

    add resizable component

--- src/main/java/idawi/service/web/react-frontend/src/App.js
+++ src/main/java/idawi/service/web/react-frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import UrlBuilder from './components/UrlBuilder';
 import ComponentContent from './components/ComponentContent';
 import Layout from './components/Layout';
+import TestResizable from './components/TestResizable';
 
 function App() {
   return (
     <div className="App">
       {/* <ComponentContent /> */}
-      <Layout />
+      {/* <Layout /> */}
+      <TestResizable />
     </div>
   );
 }
