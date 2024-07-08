89c262f7866d490139354462c5066f970b20012f
Bachir <Takeshi.caw@outlook.fr>
2024-07-08 16:36:35 +0200

    SerialTest

--- src/main/java/idawi/service/serialTest/serialTest.java
+++ src/main/java/idawi/service/serialTest/serialTest.java
@@ -8,11 +8,18 @@ public class serialTest {
         String dataToSend = "Hello, Serial Port!";
         byte[] data = dataToSend.getBytes();
         SerialPort comPort = SerialPort.getCommPort("COM7");
+        comPort.setBaudRate(57600);
+        comPort.setNumDataBits(8);
+        comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
+        comPort.setParity(SerialPort.NO_PARITY);
+
         comPort.openPort();
+
         try {
             while (true) {
                 Thread.sleep(1000);
                 int bytesWritten = comPort.writeBytes(data, data.length);
+                System.out.println(bytesWritten);
             }
         } catch (Exception e) {
             e.printStackTrace();
